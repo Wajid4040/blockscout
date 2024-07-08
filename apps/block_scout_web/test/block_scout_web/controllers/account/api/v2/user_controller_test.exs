d51b6d96df66d626d6a47c98b86ba4929816c697
Viktor Baranov <baranov.viktor.27@gmail.com>
2024-07-08 14:34:16 +0300

    chore: remove single implementation name, address from API v2 response

--- apps/block_scout_web/test/block_scout_web/controllers/account/api/v2/user_controller_test.exs
+++ apps/block_scout_web/test/block_scout_web/controllers/account/api/v2/user_controller_test.exs
@@ -210,9 +210,6 @@ defmodule BlockScoutWeb.Account.Api.V2.UserControllerTest do
              "name" => name,
              "address" => %{
                "hash" => Address.checksum(addr),
-               # todo: added for backward compatibility, remove when frontend unbound from these props
-               "implementation_address" => nil,
-               "implementation_name" => nil,
                "implementations" => [],
                "is_contract" => false,
                "is_verified" => false,
